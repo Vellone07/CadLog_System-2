05097d96bd3eda6744955268420a2b973e0d0cea
David <davibittancourt@gmail.com>
2024-10-30 08:59:44 -0300

    Atualização do user e edit user

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -23,6 +23,23 @@ class UserController
         $users = User::all();
         include 'views/list_users.php';
     }
+
+    public function edit($id){
+        session_start();
+        //permitir de admin e gestor editem usuários
+        if($_SESSION['perfil'] == 'admin' || $_SESSION['perfil'] == 'gestor'){
+            $user = User::find($id);
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                $data = [
+                    'nome'  => $_POST['nome'],
+                    'email' => $_POST['email'],
+                    'perfil'=> $_POST['perfil']
+                ];
+    User::update($id, $data);
+    header('Location: index.php?action=list');
+            }
+        }
+           
 }
 
 ?>
